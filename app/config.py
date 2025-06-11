98fcbb8415b493ad5087e189441a92fdbca84882
Talha-Rasool <talharasool450@gmail.com>
2025-06-11 12:23:43 +0200

    fix env and k8s files

--- app/config.py
+++ app/config.py
@@ -2,11 +2,11 @@
 import os
 
 class Config:
-    DB_USER = os.getenv("POSTGRES_USER")
-    DB_PASS = os.getenv("POSTGRES_PASSWORD")
+    DB_USER = os.getenv("POSTGRESQL_USER")
+    DB_PASS = os.getenv("POSTGRESQL_PASSWORD")
     DB_HOST = os.getenv("POSTGRES_HOST", "postgres")  # Default to Kubernetes service name
     DB_PORT = os.getenv("POSTGRES_PORT", "5432")
-    DB_NAME = os.getenv("POSTGRES_DB")
+    DB_NAME = os.getenv("POSTGRESQL_DATABASE")
 
     if not all([DB_USER, DB_PASS, DB_NAME]):
         raise ValueError("Missing required DB environment variables!")
